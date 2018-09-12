430de02763716e5f4669190d33efe2121ac61f49
anjuli <anjuli@10.187.187.243>
2018-09-12 10:15:10 -0700

    Committing Calculate Library to Github

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+//used to call code from calculate library
+//Anjuli Niyogi CompSci 2nd period
+//@version 0 September 6, 2018
+public class DoMath {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+	}
+
+}
---
+++ Calculate  Library/src/Calculate.java
@@ -0,0 +1,12 @@
+//this class contains the method to do various math related tasks
+//Anjuli Niyogi - CompSci 2nd period
+//@version 0 September 6, 2018
+
+public class Calculate {
+	
+	public static int square(int number) {
+	return number*number;	
+	
+	
+	}
+}
